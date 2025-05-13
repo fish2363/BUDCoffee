f266574e08507d7253879c3c24c91eabee10900c
SUSOT <a01076754042@gmail.com>
2025-05-13 20:24:59 +0900

    Feat : Save

---
+++ Assets/01.Scripts/LCM/SaveSystem/DataManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.IO;
+
+namespace _01.Scripts.LCM.SaveSystem
+{
+    
+    public class DataManager : MonoBehaviour
+    {
+        public void SaveData()
+        {
+            SaveGameData data = new SaveGameData();
+            
+            
+           // JsonUtility.ToJson(data,);
+        }
+    }
+}
---
+++ Assets/01.Scripts/LCM/SaveSystem/SaveGameData.cs
@@ -0,0 +1,9 @@
+namespace _01.Scripts.LCM.SaveSystem
+{
+    [System.Serializable]
+    public class SaveGameData
+    {
+        public int money;
+        public int maxScore;
+    }
+}
